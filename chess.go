5e39b39132d2f2f8863e41bba5142f2c88ddcd59
vnepveu <victor.nepveu@imt-atlantique.net>
2018-09-28 22:42:35 +0200

    Streamlined creation of chess pieces

--- chess.go
+++ chess.go
@@ -11,7 +11,9 @@ import (
 //}
 
 type ChessPieceType struct {
-	name      string
+	name string
+	// if the vertical variation of a move is i, and the horizontal one is j,
+	// then if i*j is not in "moves", then it is not a legal move for the piece
 	moves     []int
 	ascii_rep string
 }
@@ -19,7 +21,7 @@ type ChessPieceType struct {
 type ChessPiece struct {
 	chesspiecetype ChessPieceType
 	color          int // 0 is white 1 is black
-	id             int
+	id             int // each piece is identified by its unique id
 }
 
 /*type PieceSet struct {*/
@@ -33,27 +35,51 @@ func main() {
 	for i := 1; i < 9; i++ {
 		square_list = append(square_list, i*i, -i*i)
 	}
-	fmt.Println(square_list)
+	queen_list := append(square_list, 0)
 	bishop := ChessPieceType{"bishop", square_list, "F"}
 	knight := ChessPieceType{"knight", []int{-2, 2}, "H"}
 	king := ChessPieceType{"king", []int{-1, 0, 1}, "K"}
-	queen := ChessPieceType{"queen", []int{}, "Q"}
+	queen := ChessPieceType{"queen", queen_list, "Q"}
+	tower := ChessPieceType{"tower", []int{0}, "T"}
+	pawn := ChessPieceType{"pawn", []int{-1, 0, 1}, "P"}
+	/* chess_set := make(map[ChessPieceType]int)*/
 
-	first_white_bishop := ChessPiece{bishop, 0, 0}
-	first_white_knight := ChessPiece{knight, 0, 1}
-	white_king := ChessPiece{king, 0, 2}
-	white_queen := ChessPiece{queen, 0, 3}
-	fmt.Println(first_white_bishop)
-	chess_set := make(map[string]int)
-	// chess_set is the number of pieces of each type for a given color
-	// the following lines can be rationnalized if necessary
-	chess_set["bishop"] = 2
-	chess_set["pawn"] = 8
-	chess_set["king"] = 1
-	chess_set["queen"] = 1
-	chess_set["knight"] = 2
-	chess_set["tower"] = 2
-	fmt.Println(first_white_bishop, first_white_knight, white_king, white_queen)
+	//// chess_set is the number of pieces of each type for a given color
+	//// the following lines can be rationnalized if necessary
+	//chess_set[bishop] = 2
+	//chess_
+	//chess_set[pawn] = 8
+	//chess_set[king] = 1
+	//chess_set[queen] = 1
+	//chess_set[knight] = 2
+	//chess_set[tower] = 2
+	chess_set := make(map[int][]ChessPieceType)
+	chess_set[1] = []ChessPieceType{queen, king}
+	chess_set[2] = []ChessPieceType{bishop, knight, tower}
+	chess_set[8] = []ChessPieceType{pawn}
 
-	fmt.Println(chess_set)
+	chess_game := []ChessPiece{}
+	for number, piece_slice := range chess_set {
+		for i := 0; i < len(piece_slice); i++ {
+			piece_type := piece_slice[i]
+			for j := 0; j < number; j++ {
+				id_white := len(chess_game) + 1
+				id_black := id_white + 1
+				chess_game = append(chess_game, ChessPiece{piece_type, 0, id_white})
+				chess_game = append(chess_game, ChessPiece{piece_type, 1, id_black})
+			}
+
+		}
+	}
+	fmt.Println(len(chess_game))
+	for i := 0; i < len(chess_game); i++ {
+		fmt.Println(chess_game[i])
+	}
+
+	//first_white_bishop := ChessPiece{bishop, 0, 1}
+	//first_white_knight := ChessPiece{knight, 0, 2}
+	//white_king := ChessPiece{king, 0, 3}
+	//white_queen := ChessPiece{queen, 0, 4}
+	//first_white_pawn := ChessPiece{pawn, 0, 5}
+	/*first_white_tower := ChessPiece{tower, 0, 6}*/
 }
