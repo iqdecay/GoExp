e6835940d03cb47fb9a0a12ed96c0e6857d3d496
vnepveu <victor.nepveu@imt-atlantique.net>
2018-09-28 22:48:23 +0200

    Commented and cleaned code

--- chess.go
+++ chess.go
@@ -31,33 +31,28 @@ type ChessPiece struct {
 /*}*/
 
 func main() {
+	//Creating the complex moves
 	var square_list = []int{}
 	for i := 1; i < 9; i++ {
 		square_list = append(square_list, i*i, -i*i)
 	}
 	queen_list := append(square_list, 0)
+
+	//Creating the different types of pieces
 	bishop := ChessPieceType{"bishop", square_list, "F"}
 	knight := ChessPieceType{"knight", []int{-2, 2}, "H"}
 	king := ChessPieceType{"king", []int{-1, 0, 1}, "K"}
 	queen := ChessPieceType{"queen", queen_list, "Q"}
 	tower := ChessPieceType{"tower", []int{0}, "T"}
 	pawn := ChessPieceType{"pawn", []int{-1, 0, 1}, "P"}
-	/* chess_set := make(map[ChessPieceType]int)*/
 
-	//// chess_set is the number of pieces of each type for a given color
-	//// the following lines can be rationnalized if necessary
-	//chess_set[bishop] = 2
-	//chess_
-	//chess_set[pawn] = 8
-	//chess_set[king] = 1
-	//chess_set[queen] = 1
-	//chess_set[knight] = 2
-	//chess_set[tower] = 2
+	//Each pieces appears a certain number of time
 	chess_set := make(map[int][]ChessPieceType)
 	chess_set[1] = []ChessPieceType{queen, king}
 	chess_set[2] = []ChessPieceType{bishop, knight, tower}
 	chess_set[8] = []ChessPieceType{pawn}
 
+	//We create the game with the previously defined variables
 	chess_game := []ChessPiece{}
 	for number, piece_slice := range chess_set {
 		for i := 0; i < len(piece_slice); i++ {
@@ -65,6 +60,7 @@ func main() {
 			for j := 0; j < number; j++ {
 				id_white := len(chess_game) + 1
 				id_black := id_white + 1
+				//We create each piece for one color
 				chess_game = append(chess_game, ChessPiece{piece_type, 0, id_white})
 				chess_game = append(chess_game, ChessPiece{piece_type, 1, id_black})
 			}
@@ -75,11 +71,4 @@ func main() {
 	for i := 0; i < len(chess_game); i++ {
 		fmt.Println(chess_game[i])
 	}
-
-	//first_white_bishop := ChessPiece{bishop, 0, 1}
-	//first_white_knight := ChessPiece{knight, 0, 2}
-	//white_king := ChessPiece{king, 0, 3}
-	//white_queen := ChessPiece{queen, 0, 4}
-	//first_white_pawn := ChessPiece{pawn, 0, 5}
-	/*first_white_tower := ChessPiece{tower, 0, 6}*/
 }
