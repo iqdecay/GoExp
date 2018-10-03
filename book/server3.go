2d8b92155fdc62bd44d0de061777a4ffb6fb06ef
vnepveu <victor.nepveu@imt-atlantique.net>
2018-10-03 15:44:11 +0200

    Added server3.go, that echoes HTTP request

--- book/server3.go
+++ book/server3.go
@@ -1,4 +1,4 @@
-// server3 is a minimal lissajou and counter server
+// server3 is a minimal server that echoes http request
 package main
 
 import (
@@ -11,24 +11,24 @@ import (
 var mu sync.Mutex
 var count int
 func main() {
-	http.HandleFunc("/counter", counter)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-	handler := func(w http.ResponseWriter, r *http.Request){
-		lissajous(w)
-	}
 	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 // handler echoes the path component of the requested URL
 func handler(w http.ResponseWriter, r *http.Request){
-	mu.Lock() // ensure at most one goroutine access the counter
-	count++
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header{
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form{
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
 
-func counter(w http.ResponseWriter, r *http.Request){
-	mu.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
-	mu.Unlock()
-}
+
