ee4b3066b6e801176124981b5ad25ecfe79be2e3
vnepveu <victor.nepveu@imt-atlantique.net>
2018-10-19 11:58:52 +0200

    Added the bit modification

--- ch6/intset.go
+++ ch6/intset.go
@@ -16,10 +16,12 @@ func (s *IntSet) Has(x int) bool {
 func (s *IntSet) Remove(x int) {
 	word, bit := x/64, uint64(x%64)
 	if word < len(s.words) {
-		s.words[word] = s.words[word]&(
-
+		if s.words[word]&(1<<uint(bit)) != 0 {
+			s.words[word] -= (1<<uint(bit))
+		}
 	}
 }
+
 func (s *IntSet) Add(x int) {
 	word, bit := x/64, uint64(x%64)
 	for word >= len(s.words) {
